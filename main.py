e17eb71148f1124e04c8720b9e85a0cd7468e7c1
OmarAnwar19 <omaranwar04@outlook.com>
2022-06-24 22:14:18 -0400

    pre-coding

---
+++ main.py
@@ -0,0 +1,11 @@
+# imports
+from visualizer import run
+
+
+def main():
+    # call the run function in visualizer.py
+    run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ visualizer.py
@@ -0,0 +1,7 @@
+# imports
+import random
+
+
+#! the main function for running the app
+def run():
+    print("Sorting!")
